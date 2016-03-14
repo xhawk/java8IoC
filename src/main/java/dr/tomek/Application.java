b8859eb98a4ceab89efb5b8afb24de61f85c2797
Tomi Savolainen <tsavolai@gmail.com>
2016-03-14 10:09:41 +0200

    States is in curried function

--- src/main/java/dr/tomek/Application.java
+++ src/main/java/dr/tomek/Application.java
@@ -1,16 +1,13 @@
 package dr.tomek;
 
-import java.util.function.*;
-
 import static spark.Spark.*;
 
 public class Application {
 
 	public Application() {
 		get("/hello", (req, res) -> {
-			return Service.doo("Tomi", (result) -> {
-				return result;
-			});
+			String config = "xyz";
+			return Service.configure(config).serve("Tomi");
 		});
 
 	}
--- src/main/java/dr/tomek/Service.java
+++ src/main/java/dr/tomek/Service.java
@@ -3,11 +3,18 @@ package dr.tomek;
 import java.util.function.*;
 import java.lang.*;
 
-public class Service {
-	
-	public static String doo(String name, Function<String, String> callback) {
-		return Repository.find((greeting) -> {
-			return callback.apply(greeting + " " + name);
-		});
+public interface Service<T, R> extends Function<T, R> {
+
+	//BiFunction<String, String, String> catenator = (b, a) -> a + b;
+
+	//Function<String, Service<String, String>> currier = config -> name -> catenator.apply(config, name);
+	Function<String, Service<String, String>> currier = config -> name -> config + name;
+
+	static Service<String, String> configure(String config) {
+		return currier.apply(config);
+	}
+
+	default R serve(T term) {
+		return this.apply(term);
 	}
 }
